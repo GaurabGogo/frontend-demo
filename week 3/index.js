b6c0e76c23ff7bb13febaa0c8b9e03d8ec419bbf
gaurabgogo <gaurabgogo@gmail.com>
2025-07-01 16:10:25 +0545

    JS Event Listeners

--- week 3/index.js
+++ week 3/index.js
@@ -1,11 +1,40 @@
-function main() {
-  let a = 10;
-}
+// function main() {
+//   let a = 10;
+// }
 
-main();
-console.log(a);
+// main();
+// console.log(a);
 
+// let arr = [1, 2, 3, 4, 5];
 
-let arr = [1, 2, 3, 4, 5];
+// let person = {
+//   name: "John",
+//   age: 30,
+//   isStudent: true,
+// };
 
-arr.
+// console.log(document.getElementsByTagName("h1"));
+
+// document.getElementById("second-heading");
+
+// document.getElementsByClassName("class-heading");
+
+// console.log(document.querySelectorAll(".class-heading"));
+
+// const heading = document.getElementsByTagName("h1");
+// const heading = document.getElementById("heading");
+
+// console.log(heading.id);
+
+const box = document.getElementsByClassName("box")[0];
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  box.classList.toggle("active");
+});
+
+document.addEventListener("keyup", function (e) {
+  if (e.key === "a") {
+    box.classList.toggle("active");
+  }
+});
